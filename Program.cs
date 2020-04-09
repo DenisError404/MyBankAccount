b791108284fe66a7dd6be38f9ee1fc07c16870bf
Denis Rafi <62852838+DenisError404@users.noreply.github.com>
2020-04-09 14:21:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Denis", 80000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawal(800, DateTime.Now, "Iphone" );
+            account.MakeWithdrawal(50, DateTime.Now, "Game");
+            
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+            ////hey this is a comment
+            //// Test for a negative balance.
+            //try
+            //{
+            //    account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            //// Test that the initial balances must be positive.
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+        }
+    }
+}
